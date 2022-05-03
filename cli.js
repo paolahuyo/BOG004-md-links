2b3c9c383869e272b2a69d30aeb69c39340b69e8
Paola Huyo <di.paolahuyo@gmail.com>
2022-05-03 17:18:09 -0500

    Cambio de lugar la funcion evaluateCli desde cli a md-links y ahi si me esta reconociendo mdLinks como una funcion y realiza las validaciones

--- cli.js
+++ cli.js
@@ -1,4 +1,4 @@
-const mdlinks = require('./md-links');
+const mdLinks = require('./md-links');
 
 var inputPath = process.argv[2];
 var moreOptions = process.argv[3];
@@ -21,7 +21,7 @@ var inputOptions = () =>{
 console.log("Objeto de opciones en cli:", inputOptions());
 console.log("Path de entrada:",inputPath);
 
-// mdlinks(inputPath, inputOptions())
+// mdLinks(inputPath, inputOptions())
 // .then((objectProperties)=>{
 //   console.log(objectProperties);
 // })
@@ -29,58 +29,6 @@ console.log("Path de entrada:",inputPath);
 //   console.log("La ruta no existe");
 // });
 
-// const evaluateCli = (path, options = {}) => {
-//     if (!moreOptions){
-//         console.log("You Should enter a valid option: --validate or/and --stats to evaluate links of .md files")
-//     } else {
-//         if (options.validate) { // When the options have --validate
-//             console.log("Enter validate");
-//             mdlinks(path, inputOptions()).then((res) => {
-//                 const uniqueLinks = {};
-//                 const brokenlinks = [];
-//                 res.forEach((linkObject) => {
-//                     if (linkObject.status >= 400) {
-//                         brokenlinks.push(linkObject.status);
-//                     }
-//                     uniqueLinks[linkObject.href] = uniqueLinks[linkObject.href]
-//                     ? uniqueLinks[linkObject.href] + 1 : 1;
-//                 });
-//                 if (options.stats) { //When the option has validate and also has --stats
-//                     console.log("Total:", res.lenght);
-//                     console.log("Unique:", Object.keys(uniqueLinks).lenght);
-//                     console.log("Broken:", brokenlinks.lenght);
-//                 } else { //When the option has --validate but hasn't --stats
-//                     res.forEach((linkObject) => {
-//                         const dataStatus = `${(inputPath)} ${(linkObject.href)} ${(linkObject.status,linkObject.case)} ${(linkObject.text)}`
-//                         const noLinks = `${(inputPath)} ${(linkObject.href)}`
-//                         console.log(linkObject.href === 'There are not links' ? noLinks : dataStatus);
-//                     })
-//                 } 
-//             })
-//             .catch((err) => {
-//                 console.log(err);
-//             })   // Finish the case when the input has --validate
-//         } else if (options.stats) { //When the option only has --stats
-//             console.log("Enter stats");
-//             mdlinks(path, inputOptions())
-//                 .then((res) => {
-//                     const uniqueLinks = {};
-//                     res.forEach((linkObject) => {
-//                         uniqueLinks[linkObject.href] = uniqueLinks[linkObject.href]
-//                         ? uniqueLinks[linkObject.href] + 1 : 1;
-//                     })
-//                     console.log('Total:', res.length);
-//                     console.log('Unique:', Object.keys(uniqueLinks).length);
-//                 })
-//                 .catch((err) => {
-//                     console.log(err);
-//                 })
-
-//         }
-//     }
-// }
-
-// evaluateCli(inputPath, inputOptions())
 
 // const evaluateCli = (inputPath, inputOptions = {}) => {
 //     if (!moreOptions){
--- md-links.js
+++ md-links.js
@@ -2,8 +2,9 @@ const methods = require ("./methods.js");
 const cli = require ("./cli.js");
 
 var inputPath = process.argv[2];
+var moreOptions = process.argv[3];
 
-const mdlinks = (routeInput, optionsInput = {}) => {
+const mdLinks = (routeInput, optionsInput = {}) => {
   //console.log("entro a la funcion");
   //console.log(routeInput);
   //console.log(optionsInput.validate);
@@ -31,14 +32,66 @@ const mdlinks = (routeInput, optionsInput = {}) => {
 }
 
 //-----------Checking the promise of the function mdlinks on .then and .catch--------------///
-mdlinks(inputPath, cli.inputOptions())
-.then((objectProperties)=>{
-  console.log(objectProperties);
-})
-.catch(()=>{
-  console.log("La ruta no existe");
-});
+// mdLinks(inputPath, cli.inputOptions())
+// .then((objectProperties)=>{
+//   console.log(objectProperties);
+// })
+// .catch(()=>{
+//   console.log("La ruta no existe");
+// });
+
+const evaluateCli = (path, options = {}) => {
+  if (!moreOptions){
+      console.log("You Should enter a valid option: --validate or/and --stats to evaluate links of .md files")
+  } else {
+      if (options.validate) { // When the options have --validate
+          console.log("Enter validate");
+          mdLinks(inputPath, cli.inputOptions()).then((res) => {
+              const uniqueLinks = {};
+              const brokenlinks = [];
+              res.forEach((linkObject) => {
+                  if (linkObject.status >= 400) {
+                      brokenlinks.push(linkObject.status);
+                  }
+                  uniqueLinks[linkObject.href] = uniqueLinks[linkObject.href]
+                  ? uniqueLinks[linkObject.href] + 1 : 1;
+              });
+              if (options.stats) { //When the option has validate and also has --stats
+                  console.log("Total:", res.lenght);
+                  console.log("Unique:", Object.keys(uniqueLinks).lenght);
+                  console.log("Broken:", brokenlinks.lenght);
+              } else { //When the option has --validate but hasn't --stats
+                  res.forEach((linkObject) => {
+                      const dataStatus = `${(inputPath)} ${(linkObject.href)} ${(linkObject.status,linkObject.case)} ${(linkObject.text)}`
+                      const noLinks = `${(inputPath)} ${(linkObject.href)}`
+                      console.log(linkObject.href === 'There are not links' ? noLinks : dataStatus);
+                  })
+              } 
+          })
+          .catch((err) => {
+              console.log(err);
+          })   // Finish the case when the input has --validate
+      } else if (options.stats) { //When the option only has --stats
+          console.log("Enter stats");
+          mdLinks(inputPath, cli.inputOptions())
+            .then((res) => {
+              const uniqueLinks = {};
+              res.forEach((linkObject) => {
+                uniqueLinks[linkObject.href] = uniqueLinks[linkObject.href]
+                ? uniqueLinks[linkObject.href] + 1 : 1;
+              })
+              console.log('Total:', res.length);
+              console.log('Unique:', Object.keys(uniqueLinks).length);
+            })
+            .catch((err) => {
+              console.log(err);
+            })
+      }
+  }
+}
+
+evaluateCli(inputPath, cli.inputOptions())
 
 module.exports = {
-  mdlinks
+  mdLinks
 }
