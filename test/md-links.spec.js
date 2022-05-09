6a548da0cba5293e39e677065c2f398a93667623
Paola Huyo <di.paolahuyo@gmail.com>
2022-05-09 14:11:55 -0500

    Empece la configuracion en package.json

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,11 +1,11 @@
-const mdLinks = require('./md-links');
-const cli = require ("./cli.js");
-const methods = require ("./methods.js");
+const mdLinks = require('../md-links');
+const cli = require ("../cli.js");
+const methods = require ("../methods.js");
 
 describe('pathExists', () => {
   it('Is gonna validate if the path exists', () => {
     expect(methods.pathExists('./md-files/no-md-files/errorfile.txt')).toBeTruthy()
-    expect(methods.path_exists('./md-files/fakefile.txt')).not.toBeTruthy()
+    expect(methods.pathExists('./md-files/fakefile.txt')).not.toBeTruthy()
   });
 });
 
